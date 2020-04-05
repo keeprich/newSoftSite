c8939e73fd6434d0d445878b33dc8de4ae7ff8a0
keeprich <keeprich@keeprichs-air.lan>
2020-04-05 05:46:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// NAVIGATION START
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+// NAVIGATION ENDS
+
+
+
+
+
+
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
